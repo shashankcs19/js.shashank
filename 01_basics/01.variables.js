5b90939af1d72a02fbad185e7797b08a504224de
shashankcs19 <shashankcs2004@gmail.com>
2026-09-11 13:02:51 +0000

    added variables and datatypes

---
+++ 01_basics/01.variables.js
@@ -0,0 +1,12 @@
+const accountId = 69696969
+let accountEmail = "sha@gmail.com"
+var accountPassword = "123445"
+accountCity = "Bengeluru"
+
+/*
+prefer no to use var. 
+log for console,log. 
+console.log = "print"
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, ])
