fcd315be90225567cc3a85c8c9679fd0fc88fae6
shashankcs19 <shashankcs2004@gmail.com>
2026-09-13 02:19:41 +0000

    Strings

---
+++ 01_basics/05_String.js
@@ -0,0 +1,40 @@
+// const String = 'sop'
+// console.log(String);
+
+
+// const name = 'Shashank'
+// const repoCount = 50                                                             /// (`        IMP     `)    ` )
+
+// console.log(`My name is ${name} and my repo Count is ${repoCount}`);
+
+
+
+// const gameName = new String('                Shashank')
+// console.log(gameName[0]);
+
+// // console.log(gameName.charAt(-8,4));
+
+// // console.log(gameName.indexOf('a,k'));
+// // console.log(gameName.toUpperCase(0,4));
+
+// // const newString = gameName.substring(0,4)
+// // console.log(newString);
+
+// const anotherString = gameName.slice(4,2)
+// console.log(anotherString);
+
+const stringOne = '           Sashashan'
+console.log(stringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
